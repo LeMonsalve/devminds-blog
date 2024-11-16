ac2f57f1ccd5eb2fb86ae7bab4fb9156a864f632
Juan José Monsalve Orozco <devlemonsalve@gmail.com>
2024-11-16 17:10:18 -0500

    Refactor environment variable usage in createAdminClient function

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,15 +4,15 @@ import { Account, Client } from 'node-appwrite'
 
 export async function createAdminClient() {
   const {
-    NEXT_PUBLIC_APPWRITE_ENDPOINT,
-    NEXT_PUBLIC_APPWRITE_PROJECT,
-    NEXT_APPWRITE_API_KEY,
+    NEXT_PUBLIC_APPWRITE_ENDPOINT: endpoint,
+    NEXT_PUBLIC_APPWRITE_PROJECT: project,
+    NEXT_APPWRITE_API_KEY: key,
   } = process.env
 
   const client = new Client()
-    .setEndpoint(NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(NEXT_PUBLIC_APPWRITE_PROJECT!)
-    .setKey(NEXT_APPWRITE_API_KEY!)
+    .setEndpoint(endpoint!)
+    .setProject(project!)
+    .setKey(key!)
 
   return {
     get account() {
