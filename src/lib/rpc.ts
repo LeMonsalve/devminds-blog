56156e9ce96a6f823e53cf400ff130fc73fd6da5
Juan José Monsalve Orozco <devlemonsalve@gmail.com>
2024-12-03 16:03:09 -0500

    Update RPC client to use environment variable for app URL

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,10 @@
-import { hc } from 'hono/client'
 import { AppType } from '@/app/api/[[...route]]/route'
+import { hc } from 'hono/client'
 
-export const client = hc<AppType>('https://devminds-blog.vercel.app')
+const appUrl = process.env.NEXT_PUBLIC_APP_URL
+
+if (!appUrl) {
+  throw new Error('NEXT_PUBLIC_APP_URL is required')
+}
+
+export const client = hc<AppType>(appUrl)
